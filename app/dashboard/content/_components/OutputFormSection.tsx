7ca3d08d0b9a77af895f801782a304a46c4094ec
Chandra Shekhar <mauryashekhar9837@gmail.com>
2024-10-10 22:37:53 +0530

    added output section to see generated data

--- app/dashboard/content/_components/OutputFormSection.tsx
+++ app/dashboard/content/_components/OutputFormSection.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+'use client'
+
+import React, { useRef } from 'react'
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '../../../../theme/dark.css'
+
+import { Editor } from '@toast-ui/react-editor';
 
 const OutputFormSection = () => {
+  const editorRef:any = useRef();
+
   return (
-    <div>OutputFormSection</div>
+    <div className='shadow-lg border  rounded-lg '>
+      
+      <Editor
+      ref={editorRef}
+    initialValue="let's do something interesting"
+    // previewStyle="vertical"
+    height="600px"
+    initialEditType="wysiwyg"
+    useCommandShortcut={true}
+    onChange={()=> console.log(editorRef.current.getInstance().getMarkdown())}
+    
+  />
+
+    </div>
   )
 }
 
